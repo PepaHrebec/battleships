77c24e350843e9751fa4097737e9613ff6c01585
Josef Hrebec <josefhrebec02@gmail.com>
2022-11-07 14:21:20 +0100

    Initialize files

---
+++ src/shipBuilder.js
@@ -0,0 +1,13 @@
+const Ship = (length) => {
+  let hits = 0;
+  const len = () => {
+    return length;
+  };
+  const hit = () => {
+    hits += 1;
+  };
+  const isSunk = () => {
+    return hits === length;
+  };
+  return { len, isSunk, hit };
+};
